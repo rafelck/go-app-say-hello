43340b3266d3447a2294f926cac30c55948afb6b
rafel-akp <rafelinoxz7@gmail.com>
2023-03-30 10:45:21 +0800

    test gomodule

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rafelck/go-app-say-hello
+
+go 1.20
+
+require github.com/rafelck/go-hello v1.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_hello "github.com/rafelck/go-hello"
+)
+
+func main() {
+	fmt.Println(go_hello.SayHello())
+}
